a6074408895af928fa9b141c28bf99414380c407
josekameydev01 <gk01cracklitos@gmail.com>
2025-08-25 22:14:56 -0600

    images added - ui finished

--- RockPaperScissors2/ContentView.swift
+++ RockPaperScissors2/ContentView.swift
@@ -8,14 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var numOfQuestion = 1
+    @State private var score = 0
+    @State private var appChoice = "paper"
+    @State private var userShouldWin = false
+    
+    enum RoundResult{
+        case win, lose
+    }
+    
+    enum Moves: String, CaseIterable{
+        case paper = "paper"
+        case rock = "rock"
+        case scissors = "scissors"
+    }
+    
+    private let moves = Moves.allCases.map { $0.rawValue }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Spacer()
+                Text("App choice")
+                Text("The user should win")
+                Spacer()
+                Spacer()
+                VStack(spacing: 25) {
+                    ForEach(0..<3) { number in
+                        let currentMove = moves[number]
+                        VStack {
+                            Button {
+                                // TODO: Add functionality
+                            } label: {
+                                Image(currentMove)
+                                    .resizable()
+                                    .frame(width: 100, height: 100)
+                            }
+                            Text(currentMove)
+                                .fontWeight(.bold)
+                        }
+                    }
+                }
+                Spacer()
+                Spacer()
+                Text("\(numOfQuestion) / 10")
+                Spacer()
+            }
+            .navigationTitle("RockPaperScissors2")
         }
-        .padding()
     }
 }
 
